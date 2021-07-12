1b4866ab2a4018c1b79aa6bb10e88ce67cd56cd3
amro-kamal <amrokamal30@gmail.com>
2021-07-12 18:00:38 +0000

    added tensorboard to linearclassifier training

--- linearClassifier.py
+++ linearClassifier.py
@@ -70,43 +70,35 @@ class LC_Dataset(Dataset):
         return ( features[idx],labels[idx] )
 
 
-def train_linear_classifer(backbone_model, train_loader , val_loader, cfg):
+def train_and_validate(model, train_loader , val_loader, cfg):
     """
-       Simple training loop for PyTorch linear_classifer.
-       cfg: criterion, optimizer ,epochs , model_path='linear_classifer.ckpt' , scheduler=None  ,load_model=False, min_val_acc_to_save=88.0
+       Simple training loop for PyTorch models.
+       cfg: criterion, optimizer ,epochs , model_path='model.ckpt' , scheduler=None  ,load_model=False, min_val_acc_to_save=88.0
 
     """ 
-    if cfg['Load_model'] and cfg['load_path']:
-      print('Loading the backbone model from ckpt.....')
-      train_ckpt=torch.load(cfg['load_path'])
-      linear_classifer.load_state_dict(train_ckpt['backbone_model'])
-      print('The backbone model is ready!')
-
-
-    train_features, train_labels = predict(backbone_model(train_loader))
-    LC_train_dataset = LC_Dataset(train_features, train_labels)
-    LC_train_loader = DataLoader(LC_train_dataset,batch_size=cfg['batch_size'],shuffle=True,workers=4)
-
-    val_features, val_labels=predict(backbone_model(val_loader))
-    LC_val_loader=build_data_loader(val_features, val_labels)
-    LC_val_loader=DataLoader(LC_val_dataset,batch_size=cfg['batch_size'],shuffle=True,workers=4)
-
-    linear_classifer=nn.Sequential(nn.Linear(2048,10))
     if cfg['gpu']:
       device=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     best_val_acc=0
+    # Make sure model is in training mode.
+    if cfg['load_model'] and cfg['load_path']:
+      print('Loading the model from ckpt.....')
+      train_ckpt=torch.load(cfg['load_path'])
+      model.load_state_dict(train_ckpt['model'])
+      print('The model is ready!')
 
-    linear_classifer.train()
+
+    model.train()
     cfg['optimizer'].zero_grad()
 
-    # Move linear_classifer to the device (CPU or GPU).
-    linear_classifer.to(device)
+    # Move model to the device (CPU or GPU).
+    model.to(device)
     
     # Exponential moving average of the loss.
     ema_loss = None
     losses=[]
     train_accs=[]
     val_accs=[]
+    writer=SummaryWriter('tensorboard')
 
     print(f'----- Training on {device} -----')
     # Loop over epochs.
@@ -114,10 +106,10 @@ def train_linear_classifer(backbone_model, train_loader , val_loader, cfg):
         correct = 0
         num_examples=0
         # Loop over data.
-        loop=tqdm(enumerate(LC_train_loader , start =epoch*len(LC_train_loader)), total=len(LC_train_loader))
+        loop=tqdm(enumerate(train_loader , start =epoch*len(train_loader)), total=len(train_loader))
         for step , (images, target) in loop:
             # Forward pass.
-            output = linear_classifer(images.to(device))
+            output = model(images.to(device))
             loss = cfg['criterion'](output.to(device), target.to(device))
 
             # Backward pass.
@@ -133,15 +125,21 @@ def train_linear_classifer(backbone_model, train_loader , val_loader, cfg):
                 ema_loss = loss.item()
             else:
                 ema_loss += (loss.item() - ema_loss) * 0.01 
+
             # Compute the correct classifications
             preds = output.argmax(dim=1, keepdim=True)
             correct+= preds.cpu().eq(target.view_as(preds)).sum().item()
             num_examples+= images.shape[0]
             train_acc=correct/num_examples
+
             #tqdm
             loop.set_description(f"Epoch [{epoch+1}/{cfg['epochs']}]")
             loop.set_postfix(loss=ema_loss, acc=train_acc)
-        
+
+        #write the loss to tensorboard    
+        writer.add_scalar('train loss', ema_loss, global_step=epoch)
+        writer.add_scalar('train acc', train_acc, global_step=epoch)
+
         losses.append(ema_loss)
         train_accs.append(train_acc)
         #schedular
@@ -149,12 +147,15 @@ def train_linear_classifer(backbone_model, train_loader , val_loader, cfg):
           cfg['scheduler'].step()
         #validate
         if epoch+1 % cfg['val_period']==0:
-          val_acc = validate(linear_classifer ,LC_val_loader, device)
+          val_acc = validate(model ,val_loader, device)
+          #write the loss to tensorboard    
+          writer.add_scalar('val acc', val_acc, global_step=epoch)
+
           val_accs.append(val_acc)
           if val_acc > best_val_acc and val_acc > cfg['min_val_acc_to_save']:
-              print(f'validation accuracy increased from {best_val_acc} to {val_acc}  , saving the linear_classifer ....')
+              print(f'validation accuracy increased from {best_val_acc} to {val_acc}  , saving the model ....')
               #saving training ckpt
-              chk_point={'model_sate_dict':linear_classifer.state_dict(), 'epochs':epoch+1, 'best_val_acc':best_val_acc}
+              chk_point={'model_sate_dict':model.state_dict(), 'epochs':epoch+1, 'best_val_acc':best_val_acc}
               torch.save(chk_point, cfg['ckpt_path'])
               best_val_acc=val_acc
         print('-------------------------------------------------------------')
@@ -184,7 +185,6 @@ def validate(model, data_loader, device):
             correct += preds.cpu().eq(target.view_as(preds)).sum().item()
     model.train()
     # Print test accuracy.
-    percent = 100. * correct / len(data_loader.sampler)
-    print(f'validation accuracy: {correct} / {len(data_loader.sampler)} ({percent:.0f}%)')
-    return percent
-    
+    val_acc = 100. * correct / len(data_loader.sampler)
+    print(f'validation accuracy: {correct} / {len(data_loader.sampler)} ({val_acc:.0f}%)')
+    return val_acc
